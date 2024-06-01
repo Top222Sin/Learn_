e6fb2377749f3f65c192588c3a508bcbf8c03280
zodiac <top222sin@gmail.com>
2024-06-01 21:26:33 +0800

    demosaic done

--- src/st_test_demasaic.cpp
+++ src/st_test_demasaic.cpp
@@ -17,101 +17,6 @@ int height = 1200;
 int width = 1600;
 int channels = 1;
 
-// Function to calculate slope
-float calculate_slope(float x1, float y1, float x2, float y2) {
-    return (y2 - y1) / (x2 - x1);
-}
-
-void pad_with_zero(vector<vector<float>>& array, int pad_width) {
-    int rows = array.size();
-    int cols = array[0].size();
-
-    // 创建一个新的填充后的图像数组，大小为 (rows + 2 * pad_width) x (cols + 2 * pad_width)
-    vector<vector<float>> padded_array(rows + 2 * pad_width, vector<float>(cols + 2 * pad_width, 0));
-
-    // 复制原始数组到填充后的数组中心
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            padded_array[i + pad_width][j + pad_width] = array[i][j];
-        }
-    }
-
-    // 用0填充完成的数组赋值回原始数组
-    array.swap(padded_array);
-}
-
-void reflect_pad(vector<vector<float>>& array, int pad_width) {
-    int rows = array.size();
-    int cols = array[0].size();
-
-    // 创建一个新的填充后的图像数组
-    vector<vector<float>> padded_array(rows + 2 * pad_width, vector<float>(cols + 2 * pad_width, 0));
-
-    // 复制原始数组到填充后的数组中心
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            padded_array[pad_width + i][pad_width + j] = array[i][j];
-        }
-    }
-
-    // 顶部填充（反射填充）
-    for (int i = 0; i < pad_width; ++i) {
-        for (int j = 0; j < cols + 2 * pad_width; ++j) {
-            padded_array[pad_width - 1 - i][j] = padded_array[2 * (pad_width - 1 - i) + 1 + pad_width][j];
-        }
-    }
-
-    // 底部填充（反射填充）
-    for (int i = rows + pad_width; i < rows + 2 * pad_width; ++i) {
-        for (int j = 0; j < cols + 2 * pad_width; ++j) {
-            padded_array[i][j] = padded_array[2 * (rows + pad_width - 1) - i][j];
-        }
-    }
-
-    // 左侧和右侧填充（反射填充）
-    for (int i = 0; i < rows + 2 * pad_width; ++i) {
-        for (int j = 0; j < pad_width; ++j) {
-            padded_array[i][pad_width - 1 - j] = padded_array[i][2 * (pad_width - 1 - j) + 1 + pad_width];
-        }
-        for (int j = cols + pad_width; j < cols + 2 * pad_width; ++j) {
-            padded_array[i][j] = padded_array[i][2 * (cols + pad_width - 1) - j];
-        }
-    }
-
-    // 用填充后的数组赋值回原始数组
-    array.swap(padded_array);
-}
-
-
-// void pad_with(vector<vector<float>>& array, int pad_width) {
-//     int rows = array.size();
-//     int cols = array[0].size();
-//     printf("pad_with::rows: %d, cols: %d\n", rows, cols);
-
-//     // Top padding
-//     for (int i = 0; i < pad_width; ++i) {
-//         array.insert(array.begin(), array[0]);
-//     }
-
-//     // Bottom padding
-//     for (int i = 0; i < pad_width; ++i) {
-//         array.push_back(array[rows - 1]);
-//     }
-
-//     // Left and right padding
-//     for (int i = 0; i < rows + 2 * pad_width; ++i) {
-//         for (int j = 0; j < pad_width; ++j) {
-//             array[i].insert(array[i].begin(), array[i][0]);
-//             array[i].push_back(array[i][cols - 1]);
-//         }
-//     }
-
-//     // Update rows and cols after padding
-//     rows = array.size();
-//     cols = array[0].size();
-//     printf("pad_with::rows after padding: %d, cols after padding: %d\n", rows, cols);
-// }
-
 // 填充函数
 void pad_with(std::vector<std::vector<float>>& array, int pad_height, int pad_width) {
 int H = array.size();
@@ -141,27 +46,6 @@ int H = array.size();
     W += 2 * pad_width;
     printf("pad_with::H after padding: %d, W after padding: %d\n", H, W);
 }
-
-// 双线性插值函数
-float bilinear_interpolate(float x, float y, const std::vector<std::vector<float>>& pattern) {
-    int x1 = static_cast<int>(x);
-    int x2 = x1 + 1;
-    int y1 = static_cast<int>(y);
-    int y2 = y1 + 1;
-
-    if (x2 >= pattern[0].size()) x2 = x1;  // 处理边界情况
-    if (y2 >= pattern.size()) y2 = y1;      // 处理边界情况
-
-    float Q11 = pattern[y1][x1];
-    float Q12 = pattern[y1][x2];
-    float Q21 = pattern[y2][x1];
-    float Q22 = pattern[y2][x2];
-
-    float R1 = ((x2 - x) * Q11 + (x - x1) * Q12);
-    float R2 = ((x2 - x) * Q21 + (x - x1) * Q22);
-    return ((y2 - y) * R1 + (y - y1) * R2);
-}
-
 
 // 生成插值函数的结构
 struct Interp2D {
@@ -241,6 +125,7 @@ std::vector<std::vector<std::vector<float>>> demosaic(const std::vector<std::vec
                 current_pattern.push_back(row);
             }
 
+            // printf("current_pattern.size: %d x %d\n", current_pattern.size(), current_pattern[0].size());
             std::vector<float> x;
             std::vector<float> y;
             for (int kj = j; kj < W_pad; kj += kernel_size.second) {
